62e017e0e2d0f9bf983c37fd2aa05775225d7569
stefan-carter <stefan.carter@icloud.com>
2022-03-10 13:28:28 +0000

    .

--- Restaurants/src/App.java
+++ Restaurants/src/App.java
@@ -1,5 +1,37 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Create restaurant
+        Restaurant restaurant = new Restaurant("Stefano's Pizza");
+        // Create menu
+        Menu starters = new Menu("Starters");
+        Menu mains = new Menu("Mains");
+        Menu desserts = new Menu("Desserts");
+        // Add menu to restaurant
+        restaurant.getMenu().add(starters);
+        restaurant.getMenu().add(mains);
+        restaurant.getMenu().add(desserts);
+        // Create items
+        Item pizza1 = new Item("Farmhouse", 7.99);
+        Item pizza2 = new Item("Pepperoni", 7.99);
+        // Add items to menu
+        mains.getItem().add(pizza1);
+        mains.getItem().add(pizza2);
+
+        // Create second resaurant
+        Restaurant restaurant2 = new Restaurant("Emilie's Cakes and Shakes");
+        // Create menu
+        Menu sharers = new Menu("Sharers");
+        Menu cakes = new Menu("Cakes");
+        Menu shakes = new Menu("Shakes");
+        // Add menu to restaurant
+        restaurant2.getMenu().add(sharers);
+        restaurant2.getMenu().add(cakes);
+        restaurant2.getMenu().add(shakes);
+        // Create items
+        Item shake1 = new Item("Snickers Shake", 3.99);
+        Item shake2 = new Item("Strawberry Shake", 2.99);
+        // Add items to menu
+        shakes.getItem().add(shake1);
+        shakes.getItem().add(shake2);
     }
 }
